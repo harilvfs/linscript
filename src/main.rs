51c094f949e161aa611b7fdeb19000000f8d1bf9
Aayush Chalise <aayushchalise8@gmail.com>
2024-09-12 16:24:54 +0545

    fix grammar

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
     println!("{}", "1  󰣇 Setup Window Manager".bold().cyan());
     println!("{}", "2   Setup Vim".bold().cyan());
     println!("{}", "3   Install Neovim Plugin Manager".bold().cyan());
-    println!("{}", "4  󰖟 Install Browser".bold().cyan());
+    println!("{}", "4  󰖟 Install Browsers".bold().cyan());
     println!("{}", "5   Install Useful Packages".bold().cyan());
     println!("{}", "6   Setup GRUB".bold().cyan());
     println!("{}", "7  󰔎 Setup SDDM".bold().cyan());
